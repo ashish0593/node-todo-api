88f13d4a591eeae62a4252baab14e9332ad6a422
Ashish Verma <ashish0593@gmail.con>
2017-10-18 21:04:51 -0400

    Add DELETE /todo/:id

---
+++ playground/mongoos-remove.js
@@ -0,0 +1,17 @@
+const {ObjectId} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findOneAndRemove({_id: '59e7f60932352dc32ba685d4'}).then((todo) => {
+  console.log(todo);
+});
+
+Todo.findByIdAndRemove('59e7f60932352dc32ba685d4').then((todo) => {
+  console.log(todo);
+});
